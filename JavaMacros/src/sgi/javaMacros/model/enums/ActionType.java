4692a85c7ce4aba28ba1ec1de2deed597b5c68e5
DaBotz <dabotz.drawings@gmail.com>
2017-11-21 04:34:23 +0100

    Add files via upload

---
+++ JavaMacros/src/sgi/javaMacros/model/enums/ActionType.java
@@ -0,0 +1,11 @@
+package sgi.javaMacros.model.enums;
+
+public enum ActionType {
+	RUN_APPLICATION, 
+	SCRIPT, 
+	BUFFER, 
+	SEND, 
+	MODIFIER, 
+	VARIANT;
+
+}
---
+++ JavaMacros/src/sgi/javaMacros/model/enums/KeyDirection.java
@@ -0,0 +1,5 @@
+package sgi.javaMacros.model.enums;
+
+public enum KeyDirection {
+	UP, DOWN
+}
