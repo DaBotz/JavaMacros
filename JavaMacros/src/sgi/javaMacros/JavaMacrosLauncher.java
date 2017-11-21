37532ca14ab82be98161b974f29a1163eb338215
DaBotz <dabotz.drawings@gmail.com>
2017-11-21 04:42:44 +0100

    Add files via upload

---
+++ JavaMacros/src/sgi/javaMacros/JavaMacrosLauncher.java
@@ -0,0 +1,13 @@
+package sgi.javaMacros;
+
+import sgi.javaMacros.controller.JavaMacrosController;
+
+public class JavaMacrosLauncher {
+	public static void main(String[] args) {
+		
+		new JavaMacrosController().start(); 
+
+	}
+	
+	
+}
---
+++ JavaMacros/src/sgi/javaMacros/StringConstants.java
@@ -0,0 +1,13 @@
+package sgi.javaMacros;
+
+public interface StringConstants {
+
+	public String  NULL_STRING = "";
+	public char DOT = '.';
+	public String USER_HOME = "user.home";
+	public String VERBOSE_PROPERTY = "sgi.javaMacros.verbose";
+	public String YES = "YES";
+	public String TRUE = "TRUE";
+	
+
+}
