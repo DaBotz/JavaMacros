a5911e971cb25bcc74483ba127fb1441d74e38aa
DaBotz <dabotz.drawings@gmail.com>
2018-01-13 13:45:13 +0100

    Add files via upload

--- JavaMacros/src/sgi/javaMacros/msgs/Messages.java
+++ JavaMacros/src/sgi/javaMacros/msgs/Messages.java
@@ -3,26 +3,32 @@ package sgi.javaMacros.msgs;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import sgi.generic.ui.AbstractMsgs;
 import sgi.javaMacros.ui.JavaMacrosUI;
+import sgi.localization.AbstractMsgs;;
 
 public class Messages extends AbstractMsgs {
+	
+	
 	private static final String BUNDLE_NAME = "sgi.javaMacros.msgs.messages"; //$NON-NLS-1$
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
 
 	private Messages() {
 		super(RESOURCE_BUNDLE);
-	}
-	
-	public static final Messages M= new Messages();
+		
 
+	}
+
+	public static final Messages M = new Messages();
+
+	
 	@Override
 	public URL getSettingsImageResource() {
 		return JavaMacrosUI.class.getResource("icon.png");
 	}
 
-	public String getString(Object owner, String subkey) {
-		return getString(owner.getClass().getCanonicalName()+"."+subkey);
-	}
+
+
+	
+
 }
