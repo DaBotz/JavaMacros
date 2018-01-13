4c13f7bf52dba5d6dc12f8dd2c0d66ae76a63102
DaBotz <dabotz.drawings@gmail.com>
2018-01-13 13:32:37 +0100

    Delete IConfigAtom.java

--- JavaMacros/src/sgi/javaMacros/model/interfaces/IConfigAtom.java
+++
@@ -1,7 +0,0 @@
-package sgi.javaMacros.model.interfaces;
-
-public interface IConfigAtom{
-	public IConfigAtom getParent() ;
-	public void setParent(IConfigAtom parent);
-	public void addConfigChangeListener(IConfigChangeListener listener);
-}
