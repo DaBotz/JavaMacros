1d71fff81e6e513b3fab140f9066c300fe86670d
DaBotz <dabotz.drawings@gmail.com>
2018-01-13 13:33:07 +0100

    Delete IConfigChangeListener.java

--- JavaMacros/src/sgi/javaMacros/model/interfaces/IConfigChangeListener.java
+++
@@ -1,7 +0,0 @@
-package sgi.javaMacros.model.interfaces;
-
-import sgi.javaMacros.model.events.ConfigChangeEvent;
-
-public interface IConfigChangeListener {
-  public void handleConfigChangeEvent(ConfigChangeEvent event);
-}
